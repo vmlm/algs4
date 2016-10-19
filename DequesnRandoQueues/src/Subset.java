a47b38e317597cca39b067b3a856bf407efece2d
vmlm <vm.lara@gmail.com>
2016-10-19 12:00:55 -0500

    Used reservoir sampling as suggested by Sigmainfy at http://www.sigmainfy.com/blog/random-generate-subset-with-length-k-by-reservoir-sampling.html

--- DequesnRandoQueues/src/Subset.java
+++ DequesnRandoQueues/src/Subset.java
@@ -1,17 +1,28 @@
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Subset {
 
     public static void main(String[] args) {
-        int k = 3, j = 0;
+        int k = 3, numitems = 0, p;
         if (args.length == 1) k = Integer.parseInt(args[0]);
         RandomizedQueue<String> queue = new RandomizedQueue<String>();
         String s;
         while (!StdIn.isEmpty()) {
             s = StdIn.readString();
-            queue.enqueue(s);
+            if (numitems < k) {
+                queue.enqueue(s);
+            } else {
+                p = StdRandom.uniform(numitems+1);
+                if (p < k) {
+                    queue.dequeue();
+                    queue.enqueue(s); 
+                }
+            }
+            numitems++;
         }
+        
         for (int i = 0; i < k; i++) StdOut.println(queue.dequeue());
     }
 
